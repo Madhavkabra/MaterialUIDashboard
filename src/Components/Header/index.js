bb21f350b26a2e5670708cba2434526af0028caf
Madhav Kabra <madhav.kabra1100@gmail.com>
2020-07-12 09:08:44 +0530

    Add header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,23 +4,29 @@ import styles from './header.module.css'
 import cx from 'classnames'
 import profileImage from '../../Assets/profile.jpeg'
 
-export default function Header() {
+export default function Header({ setShowNav, showNav }) {
   const [name] = useState("Madhav kabra")
+
   return (
-    <header className={styles.headerImage}>
+    <div className={cx(styles.headerImage, { [styles.backDropShadow]: showNav })}>
       <span className={styles.logoImage}>
         <img className={styles.logo} src={Logo} alt='logo' />
-        <i className={cx("fa fa-lg fa-bars", styles.mobileMenuIcon)}></i>
+        <i
+          className={cx("fa fa-lg fa-bars", styles.mobileMenuIcon)}
+          onClick={() => setShowNav(true)}
+        ></i>
       </span>
 
       <span className={styles.iconWrapper}>
-        <i className={cx("fa fa-lg fa-search", styles.navIcons)}></i>
-        <i className={cx("fa fa-lg fa-user-o", styles.navIcons)}></i>
-        <i className={cx("fa fa-lg fa-bell-o", styles.navIcons)}></i>
-        <i className={cx("fa fa-lg fa-cog", styles.navIcons)}></i>
+        <span className={styles.iconsBlock}>
+          <i className={cx("fa fa-lg fa-search", styles.navIcons)}></i>
+          <i className={cx("fa fa-lg fa-user-o", styles.navIcons)}></i>
+          <i className={cx("fa fa-lg fa-bell-o", styles.navIcons)}></i>
+          <i className={cx("fa fa-lg fa-cog", styles.navIcons)}></i>
+        </span>
         <img className={styles.profileImage} src={profileImage} height="30px" width="30px" alt='Profile' />
         <span className={styles.userName}>{name}</span>
       </span>
-    </header>
+    </div>
   )
 }
