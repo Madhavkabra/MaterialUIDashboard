7216dd52b83181b5bd040c5071d772f1e5de8032
Madhav Kabra <madhav.kabra1100@gmail.com>
2020-07-12 09:09:23 +0530

    Add navigation, header and dashboard to layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import Header from './Components/Header';
 import NavigationBar from './Components/Navigation';
+import Dashboard from './Components/Dashboard';
+import OutsideAlerter from './Components/OutsideClickHandler';
+import styles from './app.module.css';
 
 
 function App() {
+  const [showNav, setShowNav] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  const breakpoint = 1024;
+
+  useEffect(() => {
+    window.addEventListener("resize", () => setWidth(window.innerWidth));
+  }, []);
+
+  const closeNav = () => {
+    setShowNav(false)
+  }
+
   return (
-    <div className="App">
-      <Header />
-      <NavigationBar/>
+    <div className={styles.app}>
+      <Header setShowNav={setShowNav} showNav={showNav} />
+      {width < breakpoint ?
+        <OutsideAlerter actionHandler={closeNav}>
+          <NavigationBar showNav={showNav} />
+        </OutsideAlerter>
+        : <NavigationBar showNav={showNav} />
+      }
+      <Dashboard />
     </div>
   );
 }
