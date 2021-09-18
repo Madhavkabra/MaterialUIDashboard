291ff809f4370ed591caeed3e9db0174ae77342f
Madhav Kabra <madhav.kabra1100@gmail.com>
2021-09-18 10:03:10 +0530

    Updates

---
+++ precache-manifest.015f249fef9941b0a27c89f729d00143.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ade964ff197373c40824a50ef2f4648",
+    "url": "/MaterialUIDashboard/index.html"
+  },
+  {
+    "revision": "0da2c5231fa192329a7b",
+    "url": "/MaterialUIDashboard/static/css/main.bd112ef7.chunk.css"
+  },
+  {
+    "revision": "f9f5de0b35c48616b72a",
+    "url": "/MaterialUIDashboard/static/js/2.1ad845e4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MaterialUIDashboard/static/js/2.1ad845e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0da2c5231fa192329a7b",
+    "url": "/MaterialUIDashboard/static/js/main.57de0980.chunk.js"
+  },
+  {
+    "revision": "6c49dcfd8061b7a10073",
+    "url": "/MaterialUIDashboard/static/js/runtime-main.aab3924d.js"
+  },
+  {
+    "revision": "ca4d41f3713227978e88ce7b1654464c",
+    "url": "/MaterialUIDashboard/static/media/logo.ca4d41f3.svg"
+  },
+  {
+    "revision": "7e19b77a746f786da361b43f9b38041e",
+    "url": "/MaterialUIDashboard/static/media/profile.7e19b77a.jpeg"
+  }
+]);
